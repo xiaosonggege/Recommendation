013521b8753bd92c4227d586d64a818562b00c38
xiaosonggege <1243049371@qq.com>
2020-08-07 17:18:54 +0800

    Initial commit

---
+++ model_FM/model_structure.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2020-2025.
+@contact: 1243049371@qq.com
+@software: pycharm
+@file: model_structure
+@time: 2020/8/7 5:17 下午
+'''
+
+import tensorflow as tf
+import numpy as np
+
+class Name:
+    pass
+
+
+if __name__ == '__main__':
+    pass
